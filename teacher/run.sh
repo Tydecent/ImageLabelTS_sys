b0dd6956bea805ca7be3560ae463499cd090abc9
Tydecent <Smart100430@163.com>
2026-05-18 11:20:59 +0800

    fix(teacher): 修复后台运行服务的问题
    在run.sh脚本中，将gunicorn命令前添加`nohup`并将命令放入后台运行，以确保gunicorn即使在终端关闭后也能继续运行。

--- teacher/run.sh
+++ teacher/run.sh
@@ -2,4 +2,4 @@
 source ./venv/bin/activate
 export IMAGE_DIR="./Images"
 export STUDENTS_FILE="students.txt"
-gunicorn -w 4 -k gevent --worker-connections 1000 -b 0.0.0.0:12010 --timeout 120 server:app
+nohup gunicorn -w 4 -k gevent --worker-connections 1000 -b 0.0.0.0:12010 --timeout 120 server:app &

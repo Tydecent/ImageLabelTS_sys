2e3735410f38669785c4a4418d1b72671b0f3f1e
Tydecent <Smart100430@163.com>
2026-05-18 11:19:08 +0800

    feat(teacher/run.sh): 更新服务启动脚本以使用Gunicorn
    在启动脚本中添加了环境变量 `IMAGE_DIR` 和 `STUDENTS_FILE`，分别设置为 `./Images` 和 `students.txt`。
    将服务启动方式从 `nohup python ./server.py ./Images students.txt &` 修改为 `gunicorn -w 4 -k gevent --worker-connections 1000 -b 0.0.0.0:12010 --timeout 120 server:app`，以使用Gunicorn作为WSGI服务器，增加工作进程数为4，并配置了其他相关参数以提高性能和连接能力。

--- teacher/run.sh
+++ teacher/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source ./venv/bin/activate
-nohup python ./server.py ./Images students.txt &
+export IMAGE_DIR="./Images"
+export STUDENTS_FILE="students.txt"
+gunicorn -w 4 -k gevent --worker-connections 1000 -b 0.0.0.0:12010 --timeout 120 server:app
